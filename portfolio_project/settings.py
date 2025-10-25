ba7a26e3cb771f1a036b64d75043c655ba462ede
ayagah <jacksonochieng1540@gmail.com>
2025-10-25 10:57:57 +0300

    updates 2

--- portfolio_project/settings.py
+++ portfolio_project/settings.py
@@ -132,9 +132,8 @@ STATICFILES_DIRS = [BASE_DIR / 'static']
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
-#media
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 # Email Configuration
